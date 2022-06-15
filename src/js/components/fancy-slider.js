8a92c69d8516ea04fb450abbfcad9f29c59662cf
raysofgoodness <shady5324@gmail.com>
2022-06-15 17:57:06 +0300

    first commit

---
+++ src/js/components/fancy-slider.js
@@ -0,0 +1,23 @@
+import { Fancybox } from "@fancyapps/ui";
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Toolbar: false,
+  animated: false,
+  dragToClose: false,
+
+  showClass: false,
+  hideClass: false,
+
+  closeButton: "top",
+
+  Image: {
+    click: "close",
+    wheel: "slide",
+    zoom: false,
+    fit: "cover",
+  },
+
+  Thumbs: {
+    minScreenHeight: 0,
+  },
+});
